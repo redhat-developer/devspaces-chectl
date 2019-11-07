2af89a27a68b09daa998393dbdd5e4bd628389b5
Ilya Buziuk <ibuziuk@redhat.com>
2019-11-07 09:07:01 +0100

    CRW-463 Removing unsupported installers (helm & minishift addon)
    Signed-off-by: Ilya Buziuk <ibuziuk@redhat.com>

--- src/tasks/installers/installer.ts
+++ src/tasks/installers/installer.ts
@@ -11,8 +11,6 @@
 import Command from '@oclif/command'
 import * as Listr from 'listr'
 
-import { HelmTasks } from './helm'
-import { MinishiftAddonTasks } from './minishift-addon'
 import { OperatorTasks } from './operator'
 
 export class InstallerTasks {
@@ -63,18 +61,13 @@ export class InstallerTasks {
   }
 
   installTasks(flags: any, command: Command): ReadonlyArray<Listr.ListrTask> {
-    const helmTasks = new HelmTasks()
     const operatorTasks = new OperatorTasks()
-    const minishiftAddonTasks = new MinishiftAddonTasks()
 
     let title: string
     let task: any
 
     // let task: Listr.ListrTask
-    if (flags.installer === 'helm') {
-      title = '🏃‍  Running Helm to install Che'
-      task = () => helmTasks.startTasks(flags, command)
-    } else if (flags.installer === 'operator') {
+    if (flags.installer === 'operator') {
       title = '🏃‍  Running the Che Operator'
       task = () => {
         // The operator installs Che multiuser only
@@ -85,14 +78,6 @@ export class InstallerTasks {
 
         return operatorTasks.startTasks(flags, command)
       }
-    } else if (flags.installer === 'minishift-addon') {
-      // minishift-addon supports Che singleuser only
-      if (flags.multiuser) {
-        command.warn("Che will be deployed in Single-User mode since Configured 'minishift-addon' installer which support only such.")
-        flags.multiuser = false
-      }
-      title = '🏃‍  Running the Che minishift-addon'
-      task = () => minishiftAddonTasks.startTasks(flags, command)
     } else {
       title = '🏃‍  Installer preflight check'
       task = () => { command.error(`Installer ${flags.installer} is not supported ¯\\_(ツ)_/¯`) }
