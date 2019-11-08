7c69506dc329975f4090f65ace53b112a532cdd1
David Festal <dfestal@redhat.com>
2019-11-08 21:48:21 +0100

    fix crw defaults (#8)
    Signed-off-by: David Festal <dfestal@redhat.com>

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-const DEFAULT_CHE_IMAGE = 'eclipse/che-server:7.3.2'
-const DEFAULT_CHE_OPERATOR_IMAGE = 'quay.io/eclipse/che-operator:7.3.2'
+const DEFAULT_CHE_IMAGE = 'quay.io/crw/server-rhel8:2.0'
+const DEFAULT_CHE_OPERATOR_IMAGE = 'quay.io/crw/operator-rhel8:2.0'
 
 export { DEFAULT_CHE_IMAGE, DEFAULT_CHE_OPERATOR_IMAGE }
