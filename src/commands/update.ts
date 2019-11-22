90ce8a1b7ce82a519046795c9abccd8cc6ade887
Florent BENOIT <fbenoit@redhat.com>
2019-11-22 16:31:07 +0100

    feat(update): Add update command to display download link (#22)
    Fix https://issues.jboss.org/browse/CRW-458
    
    Change-Id: Ie07ade918ca86687378539aa157783a7ad397798
    Signed-off-by: Florent Benoit <fbenoit@redhat.com>

---
+++ src/commands/update.ts
@@ -0,0 +1,22 @@
+/*********************************************************************
+ * Copyright (c) 2019 Red Hat, Inc.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ **********************************************************************/
+
+import { Command } from '@oclif/command'
+
+/**
+ * Update command to display download instructions to the user
+ */
+export default class Update extends Command {
+  static description = 'instructions for updating crwctl'
+
+  async run() {
+    this.log('To update crwctl, download the latest from https://developers.redhat.com/products/codeready-workspaces/download and install that instead of your current version.')
+  }
+}
