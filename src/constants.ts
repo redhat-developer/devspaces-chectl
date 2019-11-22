4edf39d7d2257f3a6b58df28c1f06a61689360ed
Dmytro Nochevnov <dnochevn@redhat.com>
2019-11-22 14:45:31 +0200

    Use registry.redhat.io to load server and operator (#20)
    * Use registry.redhat.io to load server and operator
    
    Fix default operator and server addresses to use registry.redhat.io by default.
    
    * Fix devfile.yaml and README.MD
    
    Signed-off-by: Dmytro Nochevnov <dnochevn@redhat.com>
    
    * Fix operator image address
    
    Signed-off-by: Dmytro Nochevnov <dnochevn@redhat.com>
    
    * Fix .gitignore
    
    Signed-off-by: Dmytro Nochevnov <dnochevn@redhat.com>
    
    * Restore README.md
    
    Signed-off-by: Dmytro Nochevnov <dnochevn@redhat.com>
    
    * fix(README): README was not updated

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-const DEFAULT_CHE_IMAGE = 'quay.io/crw/server-rhel8:2.0'
-const DEFAULT_CHE_OPERATOR_IMAGE = 'quay.io/crw/operator-rhel8:2.0'
+const DEFAULT_CHE_IMAGE = 'registry.redhat.io/codeready-workspaces/server-rhel8:2.0'
+const DEFAULT_CHE_OPERATOR_IMAGE = 'registry.redhat.io/codeready-workspaces/server-operator-rhel8:2.0'
 
 export { DEFAULT_CHE_IMAGE, DEFAULT_CHE_OPERATOR_IMAGE }
