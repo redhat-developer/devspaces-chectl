aa950bec2648811c0beefc319b12450680ea74b1
nickboldt <nboldt@redhat.com>
2020-06-18 12:33:50 -0400

    fix typo -- trailing comma
    Change-Id: I60e8c984a143ffe0f2cae132673d8a3be46066a4
    Signed-off-by: nickboldt <nboldt@redhat.com>

--- sync-chectl-to-crwctl.sh
+++ sync-chectl-to-crwctl.sh
@@ -98,7 +98,7 @@ platformString="    platform: string({\n\
 installerString="    installer: string({\n\
       char: 'a',\n\
       description: 'Installer type. If not set, default is "olm" for OpenShift >= 4.2, and "operator" for earlier versions.',\n\
-      options: ['operator', 'olm'],\n\
+      options: ['operator', 'olm']\n\
     }),"; # echo -e "$installerString"
 setPlaformDefaultsString="  static setPlaformDefaults(flags: any) {\n\
     flags.installer = 'operator'\n\
