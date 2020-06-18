71c4a1a8213ddf645e02aa65330da349fd89449d
Oleksandr Andriienko <oandriie@redhat.com>
2020-06-18 16:10:57 +0300

    Update description about default installer detection.
    Signed-off-by: Oleksandr Andriienko <oandriie@redhat.com>

--- sync-chectl-to-crwctl.sh
+++ sync-chectl-to-crwctl.sh
@@ -97,7 +97,7 @@ platformString="    platform: string({\n\
     }),"; # echo -e "$platformString"
 installerString="    installer: string({\n\
       char: 'a',\n\
-      description: 'Installer type - if OLM installed in cluster, use olm install method; else use deprecated operator method',\n\
+      description: 'Installer type. If argument is not defined, then installer will be "olm" for OpenShift with version >= 4.2, otherwise "operator".',\n\
       options: ['operator', 'olm'],\n\
     }),"; # echo -e "$installerString"
 setPlaformDefaultsString="  static setPlaformDefaults(flags: any) {\n\
