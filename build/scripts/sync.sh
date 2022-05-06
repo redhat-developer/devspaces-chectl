250e942528e061f632e264af5bbc853d319c1480
nickboldt <nboldt@redhat.com>
2022-05-06 18:35:47 -0300

    re-add crwctl readme/script
    Change-Id: Ib52f4a6ce1f2f226dbc65b7f7644189a55e61858
    Signed-off-by: nickboldt <nboldt@redhat.com>

--- build/scripts/sync.sh
+++ build/scripts/sync.sh
@@ -72,6 +72,7 @@ echo "/.github/
 /make-release.sh
 /.ci/
 /hack/
+/crwctl
 " > /tmp/rsync-excludes
 echo "Rsync ${SOURCEDIR} to ${TARGETDIR}"
 rsync -azrlt --checksum --exclude-from /tmp/rsync-excludes --delete "${SOURCEDIR}"/ "${TARGETDIR}"/
