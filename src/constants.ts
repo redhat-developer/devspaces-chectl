77d665bdc91645d7ca5c60a5a1db858d0d04c759
devstudio-release <nickboldt+devstudio-release@gmail.com>
2022-10-18 07:06:11 -0400

    ci: [sync] Push chectl @ main to devspaces-chectl @ devspaces-3-rhel-8
    Signed-off-by: devstudio-release <nickboldt+devstudio-release@gmail.com>

--- src/constants.ts
+++ src/constants.ts
@@ -12,6 +12,7 @@
 
 export const DSC_PROJECT_NAME = 'dsc'
 export const CHECTL_PROJECT_NAME = 'chectl'
+export const CHE_FLAVOR = 'devspaces'
 export const CHE_OPERATOR_TEMPLATE_DIR = 'devspaces-operator'
 export const DEVWORKSPACE_OPERATOR_TEMPLATE_DIR = 'devspaces-operator'
 
