1b1aa37e8e4ecc2f1d2f9b1e0d43ee32e11904a6
devstudio-release <nickboldt+devstudio-release@gmail.com>
2023-04-19 18:50:33 -0400

    ci: [sync] Push chectl @ 7.64.x to devspaces-chectl @ devspaces-3.6-rhel-8
    Signed-off-by: devstudio-release <nickboldt+devstudio-release@gmail.com>

--- src/utils/che.ts
+++ src/utils/che.ts
@@ -151,4 +151,10 @@ export namespace Che {
 
     throw new Error(`Route ${EclipseChe.CHE_FLAVOR} not found`)
   }
+
+  export async function isEmbeddedPluginRegistryConfigured(namespace: string): Promise<boolean> {
+    const kubeClient = KubeClient.getInstance()
+    await kubeClient.waitConfigMap(EclipseChe.PLUGIN_REGISTRY_CONFIG_MAP, namespace)
+    return await kubeClient.getConfigMapValue(EclipseChe.PLUGIN_REGISTRY_CONFIG_MAP, namespace, 'START_OPENVSX') === 'true'
+  }
 }
